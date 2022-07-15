603d71f74938c70d9af92342ff3b9336a32bd281
R3007 <r.shcherbo@gmail.com>
2022-07-15 23:41:12 +0300

    feat: i did it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    return Number(Math.abs(n).toString().split('').reverse().join(''))
 }
+
+
+//Number - return number(not string)
+//Math.abs(n) - method returns the absolute value of a number n (-1 = 1)
+//toString - method retrn a string - need for split
+//split - method splits a string into an array
+//reverse - method reverses the order of the elements in an array
+//join - method returns an array as a string
